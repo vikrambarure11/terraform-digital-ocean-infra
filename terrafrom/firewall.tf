73077acfad5c1957e1402ab746fd33831cc4f491
vikrambarure11 <vikrambarure@gmail.com>
2026-08-04 17:50:11 +0530

    Create firewall.tf

---
+++ terrafrom/firewall.tf
@@ -0,0 +1,72 @@
+resource "digitalocean_firewall" "firewall" {
+
+  name = "microservices-fw"
+
+  droplet_ids = [
+    digitalocean_droplet.vm.id
+  ]
+
+  inbound_rule {
+
+    protocol = "tcp"
+
+    port_range = "22"
+
+    source_addresses = [
+      "0.0.0.0/0"
+    ]
+  }
+
+  inbound_rule {
+
+    protocol = "tcp"
+
+    port_range = "80"
+
+    source_addresses = [
+      "0.0.0.0/0"
+    ]
+  }
+
+  inbound_rule {
+
+    protocol = "tcp"
+
+    port_range = "443"
+
+    source_addresses = [
+      "0.0.0.0/0"
+    ]
+  }
+
+  outbound_rule {
+
+    protocol = "tcp"
+
+    port_range = "1-65535"
+
+    destination_addresses = [
+      "0.0.0.0/0"
+    ]
+  }
+
+  outbound_rule {
+
+    protocol = "udp"
+
+    port_range = "1-65535"
+
+    destination_addresses = [
+      "0.0.0.0/0"
+    ]
+  }
+
+  outbound_rule {
+
+    protocol = "icmp"
+
+    destination_addresses = [
+      "0.0.0.0/0"
+    ]
+  }
+}
