b5e42bed60ef5767155a826501530a9107eea64c
vikrambarure11 <vikrambarure@gmail.com>
2026-08-04 17:50:47 +0530

    Update outputs.tf

--- terrafrom/outputs.tf
+++ terrafrom/outputs.tf
@@ -1,1 +1,19 @@
+output "public_ip" {
 
+  value = digitalocean_droplet.vm.ipv4_address
+}
+
+output "private_ip" {
+
+  value = digitalocean_droplet.vm.ipv4_address_private
+}
+
+output "droplet_id" {
+
+  value = digitalocean_droplet.vm.id
+}
+
+output "ssh_command" {
+
+  value = "ssh root@${digitalocean_droplet.vm.ipv4_address}"
+}
